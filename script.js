f811d9f0d944dbffd0ea5327c3cb456fbbb65017
slowdive <schlickjoe@gmail.com>
2020-07-17 18:33:14 -0700

    added consts for number, alpha (UC and LC), and symbol strings and arrays. confirmed they work in console.logs

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+const specStrg =  ("!\"\\#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+console.log(specStrg.length);
+const specArray = specStrg.split("");
+console.log(specArray);
+
+const numStrg = ("0123456789");
+const numArray = numStrg.split("");
+console.log(numArray);
+
+const alphaStrg = ("abcdefghijklmnopqrstuvwxyz");
+const alphaUCStrg = alphaStrg.toUpperCase();
+console.log(alphaUCStrg);
+const alphaArray = alphaStrg.split("");
+const alphaUCArray = alphaUCStrg.split("")
+console.log(alphaArray)
+console.log(alphaUCArray)
