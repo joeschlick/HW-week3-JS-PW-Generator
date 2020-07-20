384808b9a3d0025d4a0b148bba8474677f0abc87
slowdive <schlickjoe@gmail.com>
2020-07-20 00:19:00 -0700

    added random pw empty array. added for loop to add characters to pw array based on user selections. for loop runs random character generator functions created earlier

--- script.js
+++ script.js
@@ -67,11 +67,7 @@ console.log(strLength)
     }
   }
   return randPW;
-  
-
 }
-
-
 
 // console.log(generatePassword())
 
@@ -80,8 +76,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  
 }
 
 // Add event listener to generate button
