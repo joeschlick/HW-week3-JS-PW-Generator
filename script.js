519516d731ee4b7014c8972a8c9b97f36bfbc3b1
slowdive <schlickjoe@gmail.com>
2020-07-20 13:52:18 -0700

    added comments, added alert to confirm pw length selection, cleaned up code

--- script.js
+++ script.js
@@ -3,51 +3,62 @@ const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
+  // Initial prompt on button click
   let strLength = prompt("Enter desired number of characters between 8 and 128.");
+  // Makes sure a number is entered - if not, back to start
   if(isNaN(strLength)) {
     alert("Must be a number");
     return "Your Secure Password";
   }
-  if (strLength < 8 || strLength > 128) {
+  // Makes sure a number between 8 and 128 is entered - if not, back to start. If valid entry an alert confirms selection.
+  else if (strLength < 8 || strLength > 128) {
     alert("Must be a number between 8 and 128");
     return "Your Secure Password";
   }
-  
+  else {
+    alert("You selected " + strLength + " characters");
+  }
+
+  // Asks user if they want lowercase letters in the password, and confirms their selection
   let pwLcase = confirm("Include lowercase letters?");
   if (pwLcase) {
-    alert("Lowercase letters added")
-  } else {
-    alert("Lowercase letters removed")
+    alert("Lowercase letters added");
+  } 
+  else {
+    alert("Lowercase letters removed");
   }
-  
+  // Asks user if they want uppercase letters in the password, and confirms their selection
   let pwUcase = confirm("Include uppercase letters?");
   if (pwUcase) {
-    alert("Uppercase letters added")
-  } else {
-    alert("Uppercase letters removed")
+    alert("Uppercase letters added");
+  } 
+  else {
+    alert("Uppercase letters removed");
   }
-
+  // Asks user if they want numbers in the password, and confirms their selection
   let pwNum = confirm("include numbers?");
   if (pwNum) {
-    alert("Numbers added")
-  } else {
-    alert("Numbers removed")
+    alert("Numbers added");
+  } 
+  else {
+    alert("Numbers removed");
   }
-  
+  // Asks user if they want special characters in the password, and confirms their selection
   let pwSym = confirm("Include special characters?");
   if (pwSym) {
-    alert("Special characters added")
-  } else {
-    alert("Special characters removed")
+    alert("Special characters added");
+  } 
+  else {
+    alert("Special characters removed");
   }
-
+  //If user makes no selections - back to start
   if (pwLcase + pwUcase + pwNum + pwSym == 0) {
     alert("No selections made");
     return "Your Secure Password";
   }
-  
+  //Empty array to store user character group selections
   let selectionsArray = [];
-
+  //If user makes selections below strings are pushed into selectionsArray
   if (pwLcase) {
     selectionsArray.push("lower");
   }
@@ -63,34 +74,28 @@ function generatePassword() {
   if (pwSym) {
     selectionsArray.push("symbols");
   }
-
+  //Empty string to store randomly generated characters
   let randPW = "";
-
-console.log(strLength)
+  //For loop runs random character generator functions and creates random character strings that are the lenth of and contain only the character groups the user selected. Random character strings are stored in the randPW variable
   for (let i = 0; i < strLength; i++) {
     let selectionIndex = [Math.floor(Math.random()*selectionsArray.length)]
+
     if (selectionsArray[selectionIndex] == "lower") {
       randPW = randPW + randLcLetters();
-      console.log("randomly picked lower")
     }
     else if (selectionsArray[selectionIndex] == "upper") {
       randPW = randPW + randUcLetters();
-      console.log("randomly picked upper")
     }
     else if (selectionsArray[selectionIndex] == "numbers") {
       randPW = randPW + randNum();
-      console.log("randomly picked numbers")
     }
     else if (selectionsArray[selectionIndex] == "symbols") {
       randPW = randPW + randSpecChar();
-      console.log("randomly picked symbols")
     }
   }
   return randPW;
 }
-
-// console.log(generatePassword())
-
+//Runs the generatePassword function and ads the generated password to the text field on the page
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -102,29 +107,29 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-//random character generator
+//Random character generator
 const specStrg =  "!\"\\#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 const numStrg = "0123456789";
 const alphaStrg = "abcdefghijklmnopqrstuvwxyz";
 const alphaUCStrg = alphaStrg.toUpperCase();
 
-//get random symbols from string
-function randSpecChar () {
+//Get random symbols from string
+function randSpecChar() {
   return specStrg[Math.floor(Math.random()*specStrg.length)];
 }
 
-//get random numbers from string
-function randNum () {
+//Get random numbers from string
+function randNum() {
   return numStrg[Math.floor(Math.random()*numStrg.length)];
 }
 
-//get random lc letters from string
-function randLcLetters () {
+//Get random lc letters from string
+function randLcLetters() {
   return alphaStrg[Math.floor(Math.random()*alphaStrg.length)];
 }
 
-//get random uc letters from string
-function randUcLetters () {
+//Get random uc letters from string
+function randUcLetters() {
   return alphaUCStrg[Math.floor(Math.random()*alphaUCStrg.length)];
 }
   
